57d3172d289e5439f8761f5dbcd84a357d0fc24f
hoholr <128160350+hoholr@users.noreply.github.com>
2023-05-07 09:21:30 +0200

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,13 @@
+<!-- Gallery modal -->
+<div class="modal fade" id="galModal" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-body p-2">
+        <button type="button" class="close position-absolute" style="right: 1em;top: 1em;" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <img id="galImg" src="src" class="img-fluid w-100" alt="gallery-image" title="Gallery element">
+      </div>
+    </div>
+  </div>
+</div>
